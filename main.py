bcd2367f42073b9a90ac8573333be63f26e94ab7
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-25 11:56:24 +0530

    data cleaning

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 df = pd.read_csv('./TMDB - movies.csv')
-print(df.head())
+print(df)
+print(df.dtypes)
 print(f"{"=" * 40}")
 
-df["movie"] = 0
+df["budget"] = df["budget"].str.replace(',','').astype(float)
+df["revenue"] = df["revenue"].str.replace(',','').astype(float)
 print(df.head())
-
-
+print(df.dtypes)
