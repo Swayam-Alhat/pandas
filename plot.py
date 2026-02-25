d49f65d4107095e20dd9734b3ba0f78f5a5e2e71
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-25 20:27:13 +0530

    drawn simple line graph

--- plot.py
+++ plot.py
@@ -1,1 +1,10 @@
-import matplotlib 
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+y = [2,4,6,8,10]
+
+plt.plot(x,y)
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.title("First plot")
+plt.show()
