2877467edbcd5398d01b14621345b0d86f02413b
Swayam-Alhat <swayamalhat@gmail.com>
2026-02-26 14:16:57 +0530

    created line graph

--- plot.py
+++ plot.py
@@ -1,10 +1,31 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-x = [1,2,3,4,5]
-y = [2,4,6,8,10]
+# x = [1,2,3,4,5]
+# y = [1,4,9,16,25]
 
-plt.plot(x,y)
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
-plt.title("First plot")
+# plt.plot(x,y)
+# plt.xlabel("Numbers")
+# plt.ylabel("Square")
+# plt.title("Square of numbers")
+# plt.grid()
+# plt.show()
+
+# fig1,axes1 = plt.subplots()
+
+# axes1.plot(y,x)
+# axes1.set_title("New Plot")
+# axes1.set_xlabel("X-axis")
+# axes1.set_ylabel("Y-axis")
+# plt.show()
+
+x = np.linspace(0,10,100)
+y = np.sin(x)
+
+plt.figure(figsize=(8,5))
+plt.plot(x,y,label="SIN")
+plt.xlabel("X values")
+plt.ylabel("Sin values")
+plt.title("Sine wave")
+plt.grid()
 plt.show()
